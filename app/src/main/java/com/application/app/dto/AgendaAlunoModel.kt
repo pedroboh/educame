b23f32b289d95d66f1fcd8223f88985c806f93bf
Pedro Carvalho <pedroboh@live.com>
2022-09-16 19:57:28 -0300

    bug to fix: achar o adapter correto para o adaptador json no arquivo ApiService.kt
    Rfc3339DateJsonAdapter() da erro no get, mas funciona para inserir novos itens na agenda

--- app/src/main/java/com/application/app/dto/AgendaAlunoModel.kt
+++ app/src/main/java/com/application/app/dto/AgendaAlunoModel.kt
@@ -6,8 +6,8 @@ import java.util.*
 
 data class AgendaAlunoModel(
     @field:Json(name = "data") val data: Date,
-    @field:Json(name = "emailProfessor") val emailProfessor: String,
-    @field:Json(name = "nomeProfessor") val nomeProfessor: String,
+    @field:Json(name = "emailProfessor") val emailProfessor: String? = "placeholder",
+    @field:Json(name = "nomeProfessor") val nomeProfessor: String? = "placeholder",
     @field:Json(name = "nomeLive") val nomeLive: String,
     @field:Json(name = "url") val url: String
 )
